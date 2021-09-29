aa5976341fdb25851ef509f22257ae05d741da15
Jing Yi Feng <73263994+Jingy114@users.noreply.github.com>
2021-09-29 03:27:54 -0400

    Add files via upload

---
+++ APCS/pre/02/Date.java
@@ -0,0 +1,16 @@
+public class Date{
+  public static void main(String[] args){
+
+
+String day = "Sunday";
+int date = 12;
+String month = "September ";
+int year = 2021;
+
+System.out.print(day);
+System.out.print(",");
+System.out.print(month);
+System.out.print(date);
+System.out.print(",");
+System.out.println(year);
+}}
