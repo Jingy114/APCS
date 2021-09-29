aa1a65ce4ef68358c4b58f23234de4bd31ff765a
Jing Yi Feng <73263994+Jingy114@users.noreply.github.com>
2021-09-29 03:42:32 -0400

    Add files via upload

---
+++ APCS/pre/05/Methods.java
@@ -0,0 +1,16 @@
+//problem 2
+public static void isDivisible(int n, int m) {
+  if (m%n==0) {
+    System.out.println("true");
+  } else {
+    System.out.println("false");
+  }
+}
+
+//problem 3
+public static void isTriangle(int a, int b, int c) {
+
+}
+
+//problem 8
+//not sure
