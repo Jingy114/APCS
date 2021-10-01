3c77e6afce2b34df60583e92d55f165ee5cf74b0
jfeng31 <jfeng31@stuy.edu>
2021-10-01 03:12:55 -0400

    initsetup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,1 +1,18 @@
+/*
+Jing Yi Feng
+APCS
+HW08 -- Refactor Freshie Zero/Writing java code that greets specific people
+2021-09-30
+*/
+
+public class Greet {
+	public static void main(String[] args}
+		greet("Brian");
+		greet("Kaitlin");
+		greet("Emily");
+	}
+	public static void greet(String name) {
+		System.out.print("Why, hello there," + name + ". How do you do?");
+	}
+}
 
