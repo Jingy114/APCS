879bcb39baeed4560e21d0721a614dd99282825d
jfeng31 <jfeng31@stuy.edu>
2021-10-03 23:50:34 -0400

    init setup

---
+++ 09_bigsib/BigSibs.java
@@ -0,0 +1,15 @@
+/*TNPG:.. Jing Yi Feng 
+APCS
+HW09 -- What Are BigSibs Good For?/Topic/Summary... (Aim for concision, brevity>
+2021-10-01
+*/
+
+public class BigSibs {
+        public static void main(String[] args) {
+               
+        }
+        public static void greet(String name) {
+                System.out.print("Why, hello there, " + name + ". How do you do?");
+        }
+}
+
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,7 @@
+public class Greet {
+    public static void main( String[] args ) {
+        BigSibs.greet("Flim");
+        BigSibs.greet("Flam");
+        BigSibs.greet("Kazaam");
+    }
+}
