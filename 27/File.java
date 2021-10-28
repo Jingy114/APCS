d353bbdb7573afdc0de4f73d3525bf3d19917441
jfeng31 <jfeng31@stuy.edu>
2021-10-28 12:29:34 -0400

    add temp hw 27

---
+++ 27/File.java
@@ -0,0 +1,21 @@
+String fenceF(int posts) {
+	String fence = "|";
+	
+	for(int i=0; i<posts; i++) {
+		fence += "--|";
+	}
+	return fence;
+}
+
+String reverseF(String s) {
+	String result;
+	for(int start_index = s.length()-1; s >= 0; s--) {
+		result += s.substring(start_index,start_index+1);
+	}
+	return result;
+}
+
+String reverseR(String s) {
+	if s.length()
+	return s.substring(s.length()-1) + reverseR(s.substring(0,s.length()-1));
+}
