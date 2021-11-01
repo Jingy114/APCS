579d3048780968a4c2455d73be4e338ad03aa85c
jfeng31 <jfeng31@stuy.edu>
2021-11-01 00:30:58 -0400

    hw28

---
+++ cb/string-2/countHi.java
@@ -0,0 +1,20 @@
+/**
+Return the number of times that the string "hi" appears anywhere in the given string.
+**/
+
+public class countHi {
+
+public static int countHi(String str) {
+  int count = 0;
+  for (int i=0; i < str.length()-1; i++) {
+    if (str.substring(i, i+2).equals("hi"))
+    count++; }
+    return count;
+}
+
+public static void main (String args[]) {
+System.out.println(countHi("abc hi ho")); // → 1
+System.out.println(countHi("ABChi hi")); // → 2
+System.out.println(countHi("hihi")); // → 2
+	}
+}
