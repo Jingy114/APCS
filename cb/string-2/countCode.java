a0e78fb105906c0637ee37df775ade6a8edb7d09
jfeng31 <jfeng31@stuy.edu>
2021-11-01 00:31:44 -0400

    hw28

---
+++ cb/string-2/countCode.java
@@ -0,0 +1,7 @@
+/**
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+**/
+
+public class countCode {
+
+
