084b7bfbbb052a5d9ccdc624e49a418f45263d8e
jfeng31 <jfeng31@stuy.edu>
2021-11-01 00:29:37 -0400

    hw28

---
+++ cb/string-1/makeAbba.java
@@ -0,0 +1,15 @@
+/**
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+**/
+public class makeAbba {
+
+public static String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
+
+public static void main(String args[]) {
+System.out.println(makeAbba("Hi", "Bye")); // → "HiByeByeHi"
+System.out.println(makeAbba("Yo", "Alice")); // → "YoAliceAliceYo"
+System.out.println(makeAbba("What", "Up")); // → "WhatUpUpWhat"
+	}
+}
