4e4b1d3569082f121a2871038e1e67f1780bf777
jfeng31 <jfeng31@stuy.edu>
2022-03-20 23:34:39 -0400

    elevens

---
+++ cobo/elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,17 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+
+		Card test0 = new Card("Jack", "Hearts", 11);
+		Card test1 = new Card("Three", "Spade", 3);
+		Card test2 = new Card("Seven", "Diamonds", 7);
+	}
+}
